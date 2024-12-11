474614d19eab89286ec5d7cb7e0040d0f6d0fcfc
NASCC <pedro.main.tec@gmail.com>
2024-12-11 15:37:51 -0300

    [UPDATED] escapes move to up

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 mod visuals;
 
 fn main() {
+    let background_red: String = visuals::get_escape(1, 30, 41);
+    let reset: String = visuals::get_escape(0, 0, 0);
     let bold_yellow: String = visuals::get_escape(1, 33, 0);
     let bold_cyan: String = visuals::get_escape(1, 36, 0);
 
     // program begin (clear + banner)
     visuals::clear_terminal();
     visuals::print_banner((bold_yellow, bold_cyan));
-
-    let background_red: String = visuals::get_escape(1, 30, 41);
-    let reset: String = visuals::get_escape(0, 0, 0);
 
     // some text
     println!();
