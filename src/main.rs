bef02804ff649ca61ee750d5c468805a6e2b06f6
NASCC <pedro.main.tec@gmail.com>
2024-12-11 15:18:43 -0300

    [UPDATED/TESTED] new function to get colored ansi escape

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,14 @@ fn main() {
     visuals::clear_terminal();
     visuals::print_banner();
 
+    let background_red: String = visuals::get_escape(1, 30, 41);
+    let reset: String = visuals::get_escape(0, 0, 0);
+
     // some text
     println!();
-    println!("Welcome to our Advent Puzzles!!!");
+    println!(
+        "Welcome to our {} Advent Puzzles {} !!!",
+        background_red, reset
+    );
     println!();
 }
--- src/visuals/mod.rs
+++ src/visuals/mod.rs
@@ -11,6 +11,47 @@ pub fn clear_terminal() {
         .or_else(|_| Command::new("cls").status());
 }
 
+pub fn get_escape(font_style: i32, foreground: i32, background: i32) -> String {
+    let mut result: String = String::from("\x1b[0m");
+
+    let font_escope: Vec<i32> = (0..8).filter(|x| *x != 2 || *x != 5 || *x != 6).collect();
+
+    let foreground_escope: Vec<i32> = (30..38).collect();
+    let background_escope: Vec<i32> = (40..48).collect();
+
+    if font_style != 0 {
+        result.push_str(&format!(
+            "\x1b[{}m",
+            match font_escope.contains(&font_style) {
+                true => font_style,
+                _ => 4,
+            }
+        ));
+    }
+
+    if foreground != 0 {
+        result.push_str(&format!(
+            "\x1b[{}m",
+            match foreground_escope.contains(&foreground) {
+                true => foreground,
+                _ => 33,
+            }
+        ));
+    }
+
+    if background != 0 {
+        result.push_str(&format!(
+            "\x1b[{}m",
+            match background_escope.contains(&background) {
+                true => background,
+                _ => 4,
+            }
+        ));
+    }
+
+    result
+}
+
 pub fn print_banner() {
     // local values that we'll use
     use banners::{print_merged, BANNER_ESCAPE, BELLOW_TAG, RUSTVENT_OF_CODE_TITLE, TAG_ESCAPE};
