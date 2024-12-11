a31fc56936693632b5e65eef51e4e4910acf4cae
NASCC <pedro.main.tec@gmail.com>
2024-12-11 16:10:40 -0300

    [UPDATED] better color escapes naming

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,20 @@ mod visuals;
 use visuals as vs;
 
 fn main() {
-    let background_red: String = vs::get_escape(1, 30, 41);
-    let reset: String = vs::get_escape(0, 0, 0);
-    let bold_yellow: String = vs::get_escape(1, 33, 0);
-    let bold_cyan: String = vs::get_escape(1, 36, 0);
+    let reset_escape: String = vs::get_escape(0, 0, 0);
+    let bold_white_red: String = vs::get_escape(1, 37, 41);
+    let bold_yellow_none: String = vs::get_escape(1, 33, 0);
+    let bold_cyan_none: String = vs::get_escape(1, 36, 0);
 
     // program begin (clear + banner)
     vs::clear_terminal();
-    vs::print_banner((bold_yellow, bold_cyan));
+    vs::print_banner((bold_yellow_none, bold_cyan_none));
 
     // some text
     println!();
     vs::vprintln(&format!(
-        "Welcome to our {} Advent Puzzles {} !!!",
-        background_red, reset
+        "Welcome to our {} Advent Puzzles {}!!!",
+        bold_white_red, reset_escape
     ));
     println!();
 
