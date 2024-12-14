dab861c1ca4a0d2803da92d151ad336997c2c24c
NASCC <pedro.main.tec@gmail.com>
2024-12-14 11:44:24 -0300

    [UPDATED] comment on functions

--- src/visuals/mod.rs
+++ src/visuals/mod.rs
@@ -1,7 +1,9 @@
 // declare files in this module (folder)
 mod banners;
+// using struct type from puzzles mod
 use crate::puzzles::{AdventDay, Solve};
 
+// self explanatory
 pub fn clear_terminal() {
     // importing Command
     use std::process::Command;
@@ -12,16 +14,21 @@ pub fn clear_terminal() {
         .or_else(|_| Command::new("cls").status());
 }
 
+// return color escape based on ansi expected values
 pub fn get_escape(font_style: i32, foreground: i32, background: i32) -> String {
-    let mut result: String = String::from("\x1b[0m");
+    // start result with reset escape
+    let mut final_escape: String = String::from("\x1b[0m");
 
+    // collect all possible values for font, foreground and background
     let font_escope: Vec<i32> = (0..8).filter(|x| *x != 2 || *x != 5 || *x != 6).collect();
-
     let foreground_escope: Vec<i32> = (30..38).collect();
     let background_escope: Vec<i32> = (40..48).collect();
 
+    // set especific argument to 0 means 'no apply', so
+    // if the argument (foreach) isn't 0, push argument to final_escape (if in escope, else
+    // default value)
     if font_style != 0 {
-        result.push_str(&format!(
+        final_escape.push_str(&format!(
             "\x1b[{}m",
             match font_escope.contains(&font_style) {
                 true => font_style,
@@ -29,9 +36,8 @@ pub fn get_escape(font_style: i32, foreground: i32, background: i32) -> String {
             }
         ));
     }
-
     if foreground != 0 {
-        result.push_str(&format!(
+        final_escape.push_str(&format!(
             "\x1b[{}m",
             match foreground_escope.contains(&foreground) {
                 true => foreground,
@@ -39,9 +45,8 @@ pub fn get_escape(font_style: i32, foreground: i32, background: i32) -> String {
             }
         ));
     }
-
     if background != 0 {
-        result.push_str(&format!(
+        final_escape.push_str(&format!(
             "\x1b[{}m",
             match background_escope.contains(&background) {
                 true => background,
@@ -50,45 +55,56 @@ pub fn get_escape(font_style: i32, foreground: i32, background: i32) -> String {
         ));
     }
 
-    result
+    final_escape
 }
 
+// just calls another function, but is less verbose
 pub fn print_banner(banner_colors: (String, String)) {
     // local values that we'll use
-    use banners::{print_merged, BELLOW_TAG, LEFT_GAP, RUSTVENT_OF_CODE_TITLE};
+    use banners::{print_merged, BELLOW_TAG, RUSTVENT_OF_CODE_TITLE};
 
     // calling print function passing these values
     print_merged(
         &RUSTVENT_OF_CODE_TITLE,            // title banner
         BELLOW_TAG,                         // bellow tag
         (banner_colors.0, banner_colors.1), // escapes
-        (LEFT_GAP, 1),                      // banner gaps
+        (2, 1),                             // banner gaps
         (60, 0),                            // tag gaps
     );
 }
 
+// special printer to be used within solves function
 pub fn solve_print(content: &str) {
+    // split content if new lines
     let contents: Vec<String> = content.split("\n").fold(Vec::<String>::new(), |mut a, b| {
         a.push(b.to_string());
         a
     });
 
+    // for each line
     for line in contents {
+        // print arrow + line
         print!("  {}>{}  ", get_escape(1, 32, 0), get_escape(0, 0, 0));
         println!("{}", line);
     }
 }
 
+// self explanatory
 pub fn print_advent_table(advents: &[AdventDay]) {
+    // table header
     println!("  +---------+-------------------------+----------------------------+");
     println!("  |         |        Problems         |                            |");
     println!("  |  Days   +------------+------------+            Path            |");
     println!("  |         |   part 1   |   part 2   |                            |");
     println!("  +---------+------------+------------+----------------------------+");
 
+    // foreach day
     for day in advents {
+        // getting nth + solves bool
         let d_number: u8 = day.get_day_number();
         let solves: (bool, bool) = day.solves_as_bool();
+
+        // printing day number
         print!(
             "  | Day {}  | ",
             match d_number {
@@ -96,6 +112,8 @@ pub fn print_advent_table(advents: &[AdventDay]) {
                 _ => d_number.to_string(),
             }
         );
+
+        // printting each solve status
         print!(
             "{} | {}",
             if solves.0 {
@@ -109,6 +127,8 @@ pub fn print_advent_table(advents: &[AdventDay]) {
                 format!("{} UNSOLVED \x1b[0m", get_escape(1, 37, 41))
             }
         );
+
+        // printing solve path
         print!(" | ");
         print!(
             "{}src/puzzles/day{}{}",
@@ -121,18 +141,24 @@ pub fn print_advent_table(advents: &[AdventDay]) {
         );
         println!("          |");
     }
+
+    // bottom border
     println!("  +---------+-------------------------+----------------------------+");
 }
 
+// self explanatory
 pub fn print_about() {
-    println!("  About being printed");
+    println!("  Will be implemented soon");
 }
 
+// print chall txt content
 pub fn print_solve_content(solve: Option<&Solve>) {
     use banners::ROW_MAX_WIDTH;
 
+    // getting values
     let (title, content, _) = solve.unwrap().get_challenge_values();
 
+    // title + escape
     println!("\n");
     println!(
         "  {}",
@@ -152,13 +178,16 @@ pub fn print_solve_content(solve: Option<&Solve>) {
 
     println!();
 
+    // separating each row and entire content into new vectors
     let mut content_by_row: Vec<String> = Vec::new();
     let mut current_row: Vec<String> = Vec::new();
 
+    // iterate through each content line
     for row in content.unwrap().split("\n") {
+        // iterate through each word
         for word in row.split(" ") {
+            // push to current row + test (current row word count > max width)
             current_row.push(word.to_string());
-
             if current_row.iter().fold(0, |mut a, b| {
                 a += b.len();
                 a
@@ -172,11 +201,13 @@ pub fn print_solve_content(solve: Option<&Solve>) {
         current_row.clear();
     }
 
+    // print each rearranged line
     for line in content_by_row {
         println!("  {}", line);
     }
 }
 
+// self explanatory
 pub fn hide_marker() {
     use std::io::{self, Write};
 
@@ -184,6 +215,7 @@ pub fn hide_marker() {
     io::stdout().flush().unwrap();
 }
 
+// self explanatory
 pub fn show_marker() {
     use std::io::{self, Write};
 
@@ -191,13 +223,16 @@ pub fn show_marker() {
     io::stdout().flush().unwrap();
 }
 
+// simulates old videogames text
 pub fn print_animated(content: &str) {
+    // import all necessaries libs/mods
     use std::io::{self, Write};
     use std::thread;
     use std::time::Duration;
 
     io::stdout().flush().unwrap();
 
+    // do sleep print
     for c in content.chars() {
         print!("{}", c);
         io::stdout().flush().unwrap();
