4ca5894a7ac07ccab7d9ab010a5b7b20ecd59493
NASCC <pedro.main.tec@gmail.com>
2024-12-14 11:23:06 -0300

    [UPDATED] code comments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+// importing our modules and types
 mod puzzles;
 use puzzles::Solve;
 mod visuals;
@@ -6,75 +7,113 @@ use std::collections::HashMap;
 use std::io::Write;
 use visuals as vs;
 
+// enum of possible menu inputs (kinda useless > I'm using literal comparations a lot)
 enum MenuInput {
     GoToProblem,
     About,
     Quit,
 }
 
+// searching for enum value based on given input
 fn find_input(reference: &HashMap<(Key, Key), MenuInput>, to_find: Key) -> Option<&MenuInput> {
+    // foreach pair in our reference
     for (keys, value) in reference.iter() {
+        // mach searching
         match to_find {
             x if x == keys.0 => return Some(value),
             x if x == keys.1 => return Some(value),
             _ => continue,
         }
     }
+    // if not found
     None
 }
 
+// personal getch function
 fn personal_getch(holder: &Getch) -> Option<Key> {
+    // it will return an option instead Result<Key, _>
+    // easy to handle
     match holder.getch() {
         Ok(x) => Some(x),
         _ => None,
     }
 }
 
+// personal parser (str -> i32) function
 fn personal_parser(value: &str) -> Option<i32> {
+    // returns an option of i32, so, i can handle parsers with no worries about panic!() on force
+    // unwrap()
+
+    // iterate in each value char
     for c in value.chars() {
+        // if isn't digit, return None
         if !c.is_ascii_digit() {
             return None;
         }
     }
 
+    // else, force unwrap() (every char is a digit, so... no errors coming)
     Some(value.parse::<i32>().unwrap())
 }
 
+// personal input function. Works linke Python's input
 fn personal_input() -> String {
+    // imported mods/libs
     use std::io::{self, Write};
     use termion::event::Key;
     use termion::input::TermRead;
     use termion::raw::IntoRawMode;
 
+    // by clear and print on terminal, some unexpected behaviors occurred, such as:
+    // - no printing when needs to print
+    // - printing when doesn't asked to print
+    //
+    // probably because multiples io::stdout().flush().unwrap()
+    //
+    // so, I've been made this function to handle this bad behavior and it works. That's all, folks!
+
     let stdin = io::stdin();
     let mut stdout = io::stdout().into_raw_mode().unwrap();
-    let mut result: String = String::new();
+    let mut final_result: String = String::new();
 
+    // for each key you press
     for key in stdin.keys() {
+        // match it
         match key.unwrap() {
+            // if Enter, terminate
             Key::Char('\n') => break,
+            // if a printable char
             Key::Char(c) => {
+                // print into terminal + flush
                 print!("{}", c);
                 stdout.flush().unwrap();
-                result.push(c);
+                // push to final result
+                final_result.push(c);
             }
+            // if Backspace
             Key::Backspace => {
-                if !result.is_empty() {
+                // if result have +1 char => print back
+                if !final_result.is_empty() {
                     print!("\x08 \x08");
                 }
-                let _ = result.pop();
+                // pop from result + flush
+                let _ = final_result.pop();
                 stdout.flush().unwrap();
             }
+            // else, continue loop
             _ => {}
         }
     }
+    // break one line + return result
     println!();
-    result
+    final_result
 }
 
 fn main() {
+    // hidding cursor
     vs::hide_marker();
 
+    // start Getch var, alert text, menu options, solve options
     let menu_input: Getch = Getch::new();
     let mut alert: String = String::new();
     let options = [
@@ -82,23 +121,57 @@ fn main() {
         (" A ", "to print about text"),
         (" Q ", "to quit program"),
     ];
+    let solve_options = [(" R ", "to run solve"), (" ESC ", "to go back")];
 
+    // start input_maps + insert on it
     let mut input_maps: HashMap<(Key, Key), MenuInput> = HashMap::new();
     input_maps.insert((Key::Char('G'), Key::Char('g')), MenuInput::GoToProblem);
     input_maps.insert((Key::Char('A'), Key::Char('a')), MenuInput::About);
     input_maps.insert((Key::Char('Q'), Key::Char('q')), MenuInput::Quit);
 
+    // getting AdventDays collection (explained at puzzles/mod.rs)
+    let advs: Vec<puzzles::AdventDay> = puzzles::gen_advent_days();
+
+    // turning each option into a printable string (i can use a impl to do this, too)
+    let option_display: String = options
+        .iter()
+        .map(|x| {
+            let mut result: String = vs::get_escape(1, 37, 41);
+            result.push_str(x.0);
+            result.push_str(&vs::get_escape(0, 0, 0));
+            result.push(' ');
+            result.push_str(x.1);
+            result
+        })
+        .collect::<Vec<_>>()
+        .join(" | ");
+
+    // turn solve options into printable
+    let printable_options = solve_options
+        .iter()
+        .map(|x| {
+            let mut result: String = vs::get_escape(1, 37, 41);
+            result.push_str(x.0);
+            result.push_str(&vs::get_escape(0, 0, 0));
+            result.push(' ');
+            result.push_str(x.1);
+
+            result
+        })
+        .collect::<Vec<_>>()
+        .join(" | ");
+
+    // infinite menu loop while no breaks
     loop {
+        // default escapes
         let reset_escape: String = vs::get_escape(0, 0, 0);
         let bold_white_blue: String = vs::get_escape(1, 37, 44);
         let bold_yellow_none: String = vs::get_escape(1, 33, 0);
         let bold_cyan_none: String = vs::get_escape(1, 36, 0);
 
-        // program begin (clear + banner)
+        // clear + print banner
         vs::clear_terminal();
         vs::print_banner((bold_yellow_none, bold_cyan_none));
-
-        // some text
         println!();
         println!(
             "  Welcome to our {} Advent Puzzles {}!!!",
@@ -106,11 +179,11 @@ fn main() {
         );
         println!();
 
-        let advs: Vec<puzzles::AdventDay> = puzzles::gen_advent_days();
-
+        // print table with our advents puzzles
         vs::print_advent_table(&advs);
         println!();
 
+        // ask for option + print alert (print nothing if alert is empty String)
         println!(
             "  Chose an option: {}{}{}",
             vs::get_escape(1, 31, 0),
@@ -118,56 +191,59 @@ fn main() {
             vs::get_escape(0, 0, 0)
         );
 
+        // alert already printed, so... clear it
         alert.clear();
         println!();
 
-        let option_display: String = options
-            .iter()
-            .map(|x| {
-                let mut result: String = vs::get_escape(1, 37, 41);
-                result.push_str(x.0);
-                result.push_str(&vs::get_escape(0, 0, 0));
-                result.push(' ');
-                result.push_str(x.1);
-                result
-            })
-            .collect::<Vec<_>>()
-            .join(" | ");
-
+        // printing menu options
         println!("    {}", option_display);
         println!();
 
-        let catch = personal_getch(&menu_input);
+        // cath key
+        let catch: Option<Key> = personal_getch(&menu_input);
 
+        // if none (Err), redo loop
         if catch.is_none() {
             continue;
         }
 
+        // searching for Menu Input by give Key
         let opt: Option<&MenuInput> = find_input(&input_maps, catch.unwrap());
 
         match opt {
+            // if None (not found)
             None => continue,
+            // if quit
             Some(MenuInput::Quit) => break,
+            // if about
             Some(MenuInput::About) => {
+                // clear + print about
                 vs::clear_terminal();
                 vs::print_about();
 
+                // await response
                 println!("  Press anything to continue...\n");
-
                 let _ = menu_input.getch();
+
+                // redo loop
                 continue;
             }
+            // if going to problem
             Some(MenuInput::GoToProblem) => {
+                // show prompt
                 vs::print_animated("  Put here an valid Advent Day followed by the target solve, like => 4 2 means: Day 4, part 2");
                 print!("  > ");
-                vs::show_marker();
-                std::io::stdout().flush().unwrap();
 
+                // show cursor
+                vs::show_marker();
+
+                // get input + hide cursor + trim response
                 let mut response = personal_input();
                 vs::hide_marker();
                 response = response.trim().to_string();
 
-                let possible_search: Vec<Option<i32>> =
+                // turn response into a vec of 2 ints (expected)
+                let parsed_search: Vec<Option<i32>> =
                     response
                         .split_whitespace()
                         .fold(Vec::<Option<i32>>::new(), |mut a, b| {
@@ -175,33 +251,43 @@ fn main() {
                             a
                         });
 
-                match possible_search {
+                // match result
+                match parsed_search {
+                    // expected 2 elements
                     x if x.len() != 2 => {
                         alert.push_str("Invalid search input (day / solve)");
                         continue;
                     }
+                    // expected a int day
                     x if x[0].is_none() => {
                         alert.push_str("Invalid day input (numbers only)");
                         continue;
                     }
+                    // expected a int solve
                     x if x[1].is_none() => {
                         alert.push_str("Invalid solve input (numbers only)");
                         continue;
                     }
+                    // only days between 1 <=> 25
                     x if !(1..=25).contains(&x[0].unwrap()) => {
                         alert.push_str("Target day is out of scope (1 - 25)");
                         continue;
                     }
+                    // only solves between 1 <=> 2
                     x if !(1..=2).contains(&x[1].unwrap()) => {
                         alert.push_str("Target solve is out of scope (1 - 2)");
                         continue;
                     }
+                    // if all true
                     v => {
+                        // destructure elements and searchin for solve
                         vs::clear_terminal();
                         let (day, problem) = (v[0].unwrap() - 1, v[1].unwrap() - 1);
                         let found_solve: Option<&Solve> = advs[day as usize].get_solve(problem);
 
+                        // if solve is None (not found)
                         if found_solve.is_none() {
+                            // update alert
                             alert.push_str(&format!(
                                 "The target solve {}couldn't be found{} (probably unsolved/file not found)",
                                 vs::get_escape(1, 31, 0),
@@ -210,31 +296,23 @@ fn main() {
                             continue;
                         }
 
+                        // printing challenge content
                         vs::print_solve_content(found_solve);
+
+                        // prompt awaiting
                         println!("\n  ---\n");
                         println!("  You can scroll the terminal to see the entire text!\n\n");
 
-                        let solve_options = [(" R ", "to run solve"), (" ESC ", "to go back")];
-
-                        let printable_options = solve_options
-                            .iter()
-                            .map(|x| {
-                                let mut result: String = vs::get_escape(1, 37, 41);
-                                result.push_str(x.0);
-                                result.push_str(&vs::get_escape(0, 0, 0));
-                                result.push(' ');
-                                result.push_str(x.1);
-
-                                result
-                            })
-                            .collect::<Vec<_>>()
-                            .join(" | ");
-
+                        // print options
                         println!("  {}", printable_options);
 
+                        // loop
                         loop {
+                            // expecting RUN or ESC
                             match menu_input.getch() {
+                                // if RUN
                                 Ok(Key::Char(c)) if c == 'r' || c == 'R' => {
+                                    // clear terminal + (print tile | get input)
                                     vs::clear_terminal();
                                     let (title, _, input) =
                                         found_solve.unwrap().get_challenge_values();
@@ -261,8 +339,11 @@ fn main() {
                                         vs::get_escape(0, 0, 0)
                                     );
                                     println!();
+
+                                    // run solve
                                     found_solve.unwrap().run_solve(input.unwrap());
 
+                                    // expecting ESC to continue
                                     println!();
                                     println!(
                                         "  Press {} ESC {} to continue",
@@ -270,17 +351,21 @@ fn main() {
                                         vs::get_escape(0, 0, 0)
                                     );
 
+                                    // probably exists some fancy way to do this (i don't know how)
                                     loop {
                                         match menu_input.getch() {
                                             Ok(Key::Esc) => break,
                                             _ => (),
                                         }
                                     }
+                                    // go out solve loop after run
                                     break;
                                 }
+                                // go out solve loop with no run
                                 Ok(Key::Esc) => {
                                     break;
                                 }
+                                // else, do nothing
                                 _ => (),
                             }
                         }
@@ -290,5 +375,15 @@ fn main() {
         };
     }
 
+    // bye text
+    vs::clear_terminal();
+    println!();
+    println!(
+        "  {} Bye {} ^^ {}",
+        vs::get_escape(1, 37, 42),
+        vs::get_escape(1, 37, 41),
+        vs::get_escape(0, 0, 0)
+    );
+    println!();
     vs::show_marker();
 }
